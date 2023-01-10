6220fe7949aea75407bddfa158eec9db1e909872
uio23 <quantumTunnel56@icloud.com>
2023-01-10 15:38:23 +1300

    v90

--- freemart/market.py
+++ freemart/market.py
@@ -67,5 +67,5 @@ def market_page():
     items = Product.query.filter_by(listed=True).all()
     for item in items:
         loadImg(item.imagePath)
-        print(os.path.abspath(item.Path))
+        print(os.path.abspath(item.imagePath))
     return render_template("market/market.html", user=current_user, items=items)
