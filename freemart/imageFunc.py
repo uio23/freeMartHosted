063295fc3c278abccf54a844d8e3294953b93c1f
uio23 <quantumTunnel56@icloud.com>
2023-01-10 14:41:43 +1300

    v75

--- freemart/imageFunc.py
+++ freemart/imageFunc.py
@@ -22,10 +22,6 @@ def saveImg(productImage, imageFilename):
 
 def loadImg(imageFilename):
     url = f'https://raw.githubusercontent.com/uio23/freemart_img/main/{imageFilename}'
-    print(url)
     resp = requests.get(url)
-    image_io = BytesIO(resp.content)
-    img = Image(image_io)
-    img.save(image_io, 'PNG')
-    dataurl = 'data:image/png;base64,' + b64encode(image_io.getvalue()).decode('ascii')
-    return dataurl
+    img = Image(BytesIO(resp.content))
+    return img
