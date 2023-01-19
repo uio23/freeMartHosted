c711af14caf3e700cf451081a4409bd0dc3823b6
uio23 <quantumTunnel56@icloud.com>
2023-01-19 13:53:59 +1300

    ha

--- freemart/forms.py
+++ freemart/forms.py
@@ -84,10 +84,10 @@ def validate_resell(productName, newPrice, user):
 
 
 class QuizForm(FlaskForm):
-    self.response = requests.get("https://opentdb.com/api.php?amount=3&category=9&difficulty=medium&type=boolean")
-    self.raw = self.response.json()
-    self.content = self.raw['results']
-    print(self.content)
+    response = requests.get("https://opentdb.com/api.php?amount=3&category=9&difficulty=medium&type=boolean")
+    raw = self.response.json()
+    content = self.raw['results']
+    print(content)
 
     qOne = BooleanField("q1_label", validators=[validators.InputRequired(message="Please answer this question")])
     qTwo = BooleanField("q2_label", validators=[validators.InputRequired(message="Please answer this question")])
